af5fb2a9f5569252c9ec73100c8f85635238345f
Nicholas Byron <nbyron@andrew.cmu.edu>
2014-02-23 16:42:53 -0500

    added relationships and validations to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ActiveRecord::Base
+	belongs_to :category
+
+	validates_presence_of :title
+	validates_presence_of :content
+
+	scope :alphabetical, order('name')
+	scope :active, where('active = ?', true)
 end
